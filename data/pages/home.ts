3db3a3894fdb89a791158feef37720d96aa1d25f
David Vann <david@david.com>
2023-02-06 09:13:22 +0100

    CHORE: Added content

--- data/pages/home.ts
+++ data/pages/home.ts
@@ -28,6 +28,41 @@ export const HOME_PAGE_DATA : PageType = {
                 title: `What is kombucha?`,
                 varient: "h2"
             }
-        }
+        },
+        {
+            type: "heading",
+            data: {
+                title: `KOMBUCHA IS COMUNITY`,
+                varient: "h3"
+            }
+        },
+        {
+            type: "heading",
+            data: {
+                title: `KOMBUCHA FOR A MORE VIBRANT COMMUNITY`,
+                varient: "h3"
+            }
+        },
+        {
+            type: "chapter-heading",
+            data: {
+                title: `WE ARE A LOCAL BREWERY`,
+                varient: "h2"
+            }
+        },
+        {
+            type: "chapter-heading",
+            data: {
+                title: `OUR CORE VALUES`,
+                varient: "h2"
+            }
+        },
+        {
+            type: "heading",
+            data: {
+                title: `DISCOVER OUR FLAVOURS`,
+                varient: "h3"
+            }
+        },
     ]
 }
