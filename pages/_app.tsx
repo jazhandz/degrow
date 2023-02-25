9c68b1e059f1b30ca0555a96b7126c8b23377a70
David Vann <david@david.com>
2023-02-25 07:57:47 +0100

    Linked to cms

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,27 @@
 import type { AppProps } from "next/app";
 import Navigation from "../layout/Navigation";
 import Footer from "@/layout/Footer";
-import { NAVIGATION_DATA } from "@/data/navigation";
+// import { NAVIGATION_DATA } from "@/data/navigation";
+import CONTENT from "@/cms/content.json";
 import { FOOTER_DATA } from "@/data/footer";
 import GlobalStyles from "@/styles/globalStyles";
 import { useRouter } from "next/router";
 import React from "react";
+import { NavigationType } from "@/types/CMS/Navigation";
+
+const NAVIGATION_CONTENT = CONTENT.navigation as NavigationType;
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   const NAVIGATION_COLOR = React.useMemo(() => {
-    return NAVIGATION_DATA.colors.find(colorPath => router.asPath.startsWith(colorPath.path))?.color ?? undefined;
+    return NAVIGATION_CONTENT.colors.find(colorPath => router.asPath.startsWith(colorPath.path))?.color ?? undefined;
   }, [router.asPath]);
 
   return (
     <>
       <GlobalStyles />
-      <Navigation data={NAVIGATION_DATA} backgroundColor={NAVIGATION_COLOR} />
+      <Navigation data={NAVIGATION_CONTENT} backgroundColor={NAVIGATION_COLOR} />
       <main>
         <Component {...pageProps} />
       </main>
