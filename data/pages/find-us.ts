365bc9402921c57e3e778c8c8e33143f9dd38a4b
David Vann <david@david.com>
2023-02-28 20:45:19 +0100

    Update model

--- data/pages/find-us.ts
+++ data/pages/find-us.ts
@@ -1,99 +1,88 @@
 import { PageType } from "@/types/CMS/Page";
 
 export const FIND_US_PAGE_DATA: PageType = {
-  items: [
+  slug: "",
+  blocks: [
     {
       type: "section",
-      data: {
-        id: "team",
-        items: [
-          {
-            type: "heading",
-            data: {
-              title: "You can try our craft kombucha at the following places",
-              varient: "h1",
+      id: "team",
+      items: [
+        {
+          type: "heading",
+          title: "You can try our craft kombucha at the following places",
+          varient: "h1",
+        },
+        {
+          type: "chapter-heading",
+          title: "UTRECHT, NL",
+          varient: "h2",
+          color: "softBlue",
+        },
+        {
+          type: "locations",
+          locations: [
+            {
+              address: "The Village Coffee & Music<br/>Voorstraat 44-46,<br/>3512 AP Utrecht",
+              link: "https://maps.google.com",
             },
-          },
-          {
-            type: "chapter-heading",
-            data: {
-              title: "UTRECHT, NL",
-              varient: "h2",
-              color: "softBlue",
+            {
+              address: "The Village Coffee & Music<br/>Voorstraat 45-46,<br/>3512 AP Utrecht",
+              link: "https://maps.google.com",
             },
-          },
-          {
-            type: "locations",
-            data: {
-              locations: [
-                {
-                  address: "The Village Coffee & Music<br/>Voorstraat 44-46,<br/>3512 AP Utrecht",
-                  link: "https://maps.google.com",
-                },
-                {
-                  address: "The Village Coffee & Music<br/>Voorstraat 45-46,<br/>3512 AP Utrecht",
-                  link: "https://maps.google.com",
-                },
-                {
-                  address: "The Village Coffee & Music<br/>Voorstraat 46-46,<br/>3512 AP Utrecht",
-                  link: "https://maps.google.com",
-                },
-                {
-                  address: "The Village Coffee & Music<br/>Voorstraat 47-46,<br/>3512 AP Utrecht",
-                  link: "https://maps.google.com",
-                },
-                {
-                  address: "The Village Coffee & Music<br/>Voorstraat 48-46,<br/>3512 AP Utrecht",
-                  link: "https://maps.google.com",
-                },
-                {
-                  address: "The Village Coffee & Music<br/>Voorstraat 49-46,<br/>3512 AP Utrecht",
-                  link: "https://maps.google.com",
-                },
-              ],
+            {
+              address: "The Village Coffee & Music<br/>Voorstraat 46-46,<br/>3512 AP Utrecht",
+              link: "https://maps.google.com",
             },
-          },
-          {
-            type: "chapter-heading",
-            data: {
-              title: "AMSTERDAM, NL",
-              varient: "h2",
-              color: "softGreen",
+            {
+              address: "The Village Coffee & Music<br/>Voorstraat 47-46,<br/>3512 AP Utrecht",
+              link: "https://maps.google.com",
             },
-          },
-          {
-            type: "locations",
-            data: {
-              locations: [
-                {
-                  address: "The Village Coffee & Music<br/>Voorstraat 44-46,<br/>3512 AP Utrecht",
-                  link: "https://maps.google.com",
-                },
-                {
-                  address: "The Village Coffee & Music<br/>Voorstraat 45-46,<br/>3512 AP Utrecht",
-                  link: "https://maps.google.com",
-                },
-                {
-                  address: "The Village Coffee & Music<br/>Voorstraat 46-46,<br/>3512 AP Utrecht",
-                  link: "https://maps.google.com",
-                },
-                {
-                  address: "The Village Coffee & Music<br/>Voorstraat 47-46,<br/>3512 AP Utrecht",
-                  link: "https://maps.google.com",
-                },
-                {
-                  address: "The Village Coffee & Music<br/>Voorstraat 48-46,<br/>3512 AP Utrecht",
-                  link: "https://maps.google.com",
-                },
-                {
-                  address: "The Village Coffee & Music<br/>Voorstraat 49-46,<br/>3512 AP Utrecht",
-                  link: "https://maps.google.com",
-                },
-              ],
+            {
+              address: "The Village Coffee & Music<br/>Voorstraat 48-46,<br/>3512 AP Utrecht",
+              link: "https://maps.google.com",
             },
-          },
-        ],
-      },
+            {
+              address: "The Village Coffee & Music<br/>Voorstraat 49-46,<br/>3512 AP Utrecht",
+              link: "https://maps.google.com",
+            },
+          ],
+        },
+        {
+          type: "chapter-heading",
+          title: "AMSTERDAM, NL",
+          varient: "h2",
+          color: "softGreen",
+        },
+        {
+          type: "locations",
+          locations: [
+            {
+              address: "The Village Coffee & Music<br/>Voorstraat 44-46,<br/>3512 AP Utrecht",
+              link: "https://maps.google.com",
+            },
+            {
+              address: "The Village Coffee & Music<br/>Voorstraat 45-46,<br/>3512 AP Utrecht",
+              link: "https://maps.google.com",
+            },
+            {
+              address: "The Village Coffee & Music<br/>Voorstraat 46-46,<br/>3512 AP Utrecht",
+              link: "https://maps.google.com",
+            },
+            {
+              address: "The Village Coffee & Music<br/>Voorstraat 47-46,<br/>3512 AP Utrecht",
+              link: "https://maps.google.com",
+            },
+            {
+              address: "The Village Coffee & Music<br/>Voorstraat 48-46,<br/>3512 AP Utrecht",
+              link: "https://maps.google.com",
+            },
+            {
+              address: "The Village Coffee & Music<br/>Voorstraat 49-46,<br/>3512 AP Utrecht",
+              link: "https://maps.google.com",
+            },
+          ],
+        },
+      ],
     },
   ],
 };
