b73c1431ebb13f61c3c41705cff5a86c027b7401
David Vann <david@david.com>
2023-03-18 17:23:17 +0100

    Updated Meta data for language and connected default cms metadata

--- components/SEO/index.tsx
+++ components/SEO/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { Constants } from "@/constants";
+import Content from "@/cms/content.json";
 
 interface SEOProps {
   title?: string;
@@ -9,36 +9,22 @@ interface SEOProps {
 }
 
 export function SEO({ description, lang, meta = [], title }: SEOProps) {
-  const metaDescription = description || Constants.metaData.description;
+  const metaTitle = title || Content?.defaults?.seo?.title;
+  const metaDescription = description || Content?.defaults?.seo?.description;
 
   return (
     <Head>
       <link rel="icon" href="/favicon.ico" />
       <title>{title}</title>
-      <meta
-        property="description"
-        content={metaDescription}
-        key="description"
-      />
-      <meta property="og:title" content={title} key="og:title" />
-      <meta
-        property="og:description"
-        content={metaDescription}
-        key="og:description"
-      />
+      <meta property="description" content={metaDescription} key="description" />
+      <meta property="content-language" content="en-us"></meta>
+      <meta property="og:title" content={metaTitle} key="og:title" />
+      <meta property="og:description" content={metaDescription} key="og:description" />
       <meta property="og:type" content="website" key="og:type" />
       <meta property="twitter:card" content="summary" key="twitter:card" />
-      <meta
-        property="twitter:creator"
-        content={Constants.metaData.author}
-        key="twitter:creator"
-      />
-      <meta property="twitter:title" content={title} key="twitter:title" />
-      <meta
-        property="twitter:description"
-        content={metaDescription}
-        key="twitter:description"
-      />
+      {/* <meta property="twitter:creator" content={Constants.metaData.author} key="twitter:creator" /> */}
+      <meta property="twitter:title" content={metaTitle} key="twitter:title" />
+      <meta property="twitter:description" content={metaDescription} key="twitter:description" />
     </Head>
   );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 import Document from "next/document";
 import { ServerStyleSheet } from "styled-components";
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   // Support for styled-components in nextjs
@@ -11,8 +11,7 @@ export default class MyDocument extends Document {
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
         });
 
       const initialProps = await Document.getInitialProps(ctx);
@@ -32,17 +31,19 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
-          <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap" rel="stylesheet"/>
-          <style>
-          </style>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap"
+            rel="stylesheet"
+          />
+          <style></style>
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
