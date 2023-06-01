d6fe1a52645e6417af58e8f97471b7a21e580a86
David Vann <david@david.com>
2023-06-01 13:25:45 +0200

    FIx build

--- data/footer.ts
+++ data/footer.ts
@@ -1,30 +1,32 @@
 import { FooterType } from "@/types/CMS/Footer";
 
-export const FOOTER_DATA : FooterType = {
-    "title": "DEGR0W LAB",
-    homePath: "/",
-    contact: {
-        title: "Contact",
-        body: "Saffierlaan 10,<br/>3523RC Utrecht<br/>The Netherlands"
-    },
+export const FOOTER_DATA: any = {
+  title: "DEGR0W LAB",
+  homePath: "/",
+  contact: {
+    title: "Contact",
+    body: "Saffierlaan 10,<br/>3523RC Utrecht<br/>The Netherlands",
+  },
+  links: [
+    { label: "Email", path: "mailto: example@degrowlab.nl" },
+    { label: "Instagram", path: "https://www.instagram.com/degrowlab/" },
+    { label: "Linkedin", path: "https://www.linkedin.com/company/de-grow-lab" },
+  ],
+  navigation: {
+    title: "Other",
     links: [
-        {label: "Email", path: "mailto: example@degrowlab.nl"},
-        {label: "Instagram", path: "https://www.instagram.com/degrowlab/"},
-        {label: "Linkedin", path: "https://www.linkedin.com/company/de-grow-lab"},
+      {
+        label: "FAQ",
+        path: "/faq",
+      },
+      {
+        label: "Privacy policy",
+        path: "/privacy-policy",
+      },
+      {
+        label: "Terms & Cond",
+        path: "/terms-and-conditions",
+      },
     ],
-    navigation: {
-        title: "Other",
-        links: [{
-            label: "FAQ",
-            path: "/faq"
-        },
-        {
-            label: "Privacy policy",
-            path: "/privacy-policy"
-        },
-        {
-            label: "Terms & Cond",
-            path: "/terms-and-conditions"
-        }]
-    }
-}
+  },
+};
